2153e4d25302c602c637e66bf75c86f1ce0c22c8
vitpinheiro <120499203+vitpinheiro@users.noreply.github.com>
2025-07-21 14:25:53 +0000

    teste

---
+++ src/frontend/src/app/meuspedidos/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "PowerUP - Meus Pedidos", 
+};
+
+export default function MeusPedidos() {
+  return (
+    <main>
+      Meus Pedidos
+    </main>
+  );
+}
