b5ece9bc2373a3bc647ed4bb9d93a2a598998daf
vitpinheiro <120499203+vitpinheiro@users.noreply.github.com>
2025-07-24 14:49:15 +0000

    página de cadastro responsiva

--- src/frontend/src/app/(auth)/cadastro/page.tsx
+++ src/frontend/src/app/(auth)/cadastro/page.tsx
@@ -4,18 +4,41 @@ import { Button } from "@/components/ui/button";
 export default function CadastroPage() {
     return (
         <main className="flex flex-row-reverse relative h-screen">
-            <section className="relative nt-sm:w-[60%] w-full flex flex-col justify-center items-center bg-white rounded-l-3xl z-20">
-                <div className="nt-lg:w-[50%] w-3/4 space-y-15 flex flex-col items-center">
-                    <h3 className="mb-lg:text-2xl text-xl text-center font-semibold">BEM VINDO DE VOLTA!</h3>
-                    <form action="" className="w-full space-y-15">
+        <section className="relative nt-sm:w-[60%] w-full flex flex-col justify-center items-center bg-white rounded-l-3xl z-20 ">
+                <div className="nt-lg:w-[50%] w-3/4 space-y-15 flex flex-col items-center  mt-20">
+                    <form action="" className="w-full space-y-15 mt-5 mb-5">
                         <div className="space-y-7">
                             <div className="flex flex-col gap-2">
-                                <label htmlFor="email" className="font-semibold mb-lg:text-lg">E-MAIL</label>
-                                <input type="email" name="email" id="email" placeholder="Digite seu e-mail" className="input"/>
+                                <label htmlFor="nome" className="font-semibold mb-lg:text-lg">NOME COMPLETO</label>
+                                <input type="text" name="nome" id="nome" placeholder="Digite seu nome" className="input w-full"/>
                             </div>
-                            <div className="flex flex-col gap-2">
-                                <label htmlFor="senha" className="font-semibold mb-lg:text-lg">SENHA</label>
-                                <input type="password" name="senha" id="senha" placeholder="Digite sua senha" className="input"/>
+                            <div className="flex flex-col gap-4">
+                                <label htmlFor="email" className="font-semibold mb-lg:text-lg">E-MAIL</label>
+                                <input type="email" name="email" id="email" placeholder="Digite seu email" className="input w-full" />
+                            </div>
+                         
+                            <div className="w-full flex flex-col md:flex-row md:space-x-24 space-y-6 md:space-y-0 items-start">
+                                <div className="flex flex-col gap-2 w-full md:w-1/2">
+                                    <label htmlFor="cpf" className="font-semibold mb-lg:text-lg whitespace-nowrap">CPF</label>
+                                    <input type="text" name="cpf" id="cpf" placeholder="Digite seu CPF" className="input w-full" />
+                                </div>
+
+                                <div className="flex flex-col gap-2 w-full md:w-1/2">
+                                    <label htmlFor="telefone" className="font-semibold mb-lg:text-lg whitespace-nowrap">TELEFONE CELULAR</label>
+                                    <input type="text" name="telefone" id="telefone" placeholder="Digite seu telefone celular" className="input w-full" />
+                                </div>
+                            </div>
+
+                            <div className="w-full flex flex-col md:flex-row md:space-x-24 space-y-6 md:space-y-0 items-start">
+                                <div className="flex flex-col gap-2 w-full md:w-1/2">
+                                    <label htmlFor="senha" className="font-semibold mb-lg:text-lg whitespace-nowrap">SENHA</label>
+                                    <input type="text" name="senha" id="senha" placeholder="Digite sua senha" className="input w-full" />
+                                </div>
+
+                                <div className="flex flex-col gap-2 w-full md:w-1/2">
+                                    <label htmlFor="telefone2" className="font-semibold mb-lg:text-lg whitespace-nowrap">CONFIRMAÇÃO DA SENHA</label>
+                                    <input type="text" name="telefone2" id="telefone2" placeholder="Digite sua senha novamente" className="input w-full" />
+                                </div>
                             </div>
                         </div>
                         <Button type="submit" variant="submit" size="submit">
