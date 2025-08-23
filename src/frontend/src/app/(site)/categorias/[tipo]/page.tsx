44b7bd6e0bc1e8c0998e88fb584c09434a690fe7
lucasbrito0611 <lucasbrito2006@hotmail.com>
2025-08-23 16:10:24 -0300

    Correção de erros de async com params

--- src/frontend/src/app/(site)/categorias/[tipo]/page.tsx
+++ src/frontend/src/app/(site)/categorias/[tipo]/page.tsx
@@ -1,16 +1,15 @@
 import { Metadata } from "next";
 import CategoriaClient from "./CategoriaClient";
 
-export async function generateMetadata({ params }: { params: { tipo: string } }): Promise<Metadata> {
-  const { tipo } = params;
+export async function generateMetadata({ params }: { params: Promise<{ tipo: string }> }): Promise<Metadata> {
+  const { tipo } = await params;
 
   return {
     title: `PowerUP - ${tipo.charAt(0).toUpperCase() + tipo.slice(1)}`,
   };
 }
 
-const Categoria = ({ params }: { params: { tipo: string } }) => {
-  return <CategoriaClient tipo={params.tipo}/>
+export default async function CategoriaPage({ params }: { params: Promise<{ tipo: string }> }) {
+  const { tipo } = await params; 
+  return <CategoriaClient tipo={tipo} />;
 }
-
-export default Categoria;
